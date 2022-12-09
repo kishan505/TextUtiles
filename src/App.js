f1efef30d8fc45607d8940a13af08990e55e796a
kishan <gondaliyak003@gmail.com>
2022-12-09 23:23:19 +0530

    enable dark and light mode in about component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
-import TextForm from "./Components/TextForm";
+// import TextForm from "./Components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="container my-3">
-        <TextForm heading="Enter The text to Analyze Below" />
+        {/* <TextForm heading="Enter The text to Analyze Below" /> */}
+        <About />
       </div>
     </>
   );
